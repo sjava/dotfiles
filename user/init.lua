de3c9b2fd187550311d7c08152ab4849636ab23a
sjava <sjava@163.com>
2022-09-23 22:47:42 +0800

    remove smooth cursor plugin.

--- user/init.lua
+++ user/init.lua
@@ -65,7 +65,6 @@ local config = {
       },
       {"h-hg/fcitx.nvim"},
       {"sjava/yode-nvim", config = function() require("yode-nvim").setup({}) end},
-      {'gen740/SmoothCursor.nvim', config = function() require('smoothcursor').setup() end},
       {
         "echasnovski/mini.nvim",
         config = function()
