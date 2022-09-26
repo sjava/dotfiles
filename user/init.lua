a2c5ec1e30daff9231ed7cee8e34dabe5c73bd8b
sjava <515957966@qq.com>
2022-09-26 10:23:31 +0800

    set tab key for copilot.

--- user/init.lua
+++ user/init.lua
@@ -120,6 +120,26 @@ local config = {
         config = function()
           require("copilot_cmp").setup()
           astronvim.add_cmp_source({name = "copilot", priority = 1500, group_index = 1})
+
+          local has_words_before = function()
+            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and
+                     vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") ==
+                     nil
+          end
+          local cmp = require "cmp"
+          cmp.setup({
+            mapping = {
+              ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                if cmp.visible() and has_words_before() then
+                  cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+                else
+                  fallback()
+                end
+              end)
+            }
+          })
         end
       },
       {"sbdchd/neoformat"},
