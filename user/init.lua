83696875f923acd469cc696dbc68b4f8953d5193
sjava <515957966@qq.com>
2022-10-19 09:25:13 +0800

    update cmp-copilot config

--- user/init.lua
+++ user/init.lua
@@ -397,6 +397,9 @@ local config = {
 				config = function()
 					require("copilot_cmp").setup({
 						method = "getCompletionsCycling",
+						formatters = {
+							insert_text = require("copilot_cmp.format").remove_existing,
+						},
 					})
 					astronvim.add_cmp_source({ name = "copilot", priority = 1500 })
 
