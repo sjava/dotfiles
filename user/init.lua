2542d46d33e292cf42b64a3b95ccbb886ed0a459
sjava <515957966@qq.com>
2022-11-17 17:03:52 +0800

    add rust snippets

--- user/init.lua
+++ user/init.lua
@@ -578,7 +578,7 @@ local config = {
 	-- LuaSnip Options
 	luasnip = {
 		-- Add paths for including more VS Code style snippets in luasnip
-		vscode_snippet_paths = {},
+		vscode_snippet_paths = { "./lua/user/rusty-snippy" },
 		-- Extend filetypes
 		filetype_extend = {
 			-- javascript = { "javascriptreact" },
