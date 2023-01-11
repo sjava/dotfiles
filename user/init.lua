06d97695ec5572ab52bd7bc89aa0fea46c5b2dda
sjava <515957966@qq.com>
2023-01-11 09:15:43 +0800

    update

--- user/init.lua
+++ user/init.lua
@@ -4,7 +4,8 @@ local config = {
 	-- Configure AstroNvim updates
 	updater = {
 		remote = "origin", -- remote to use
-		channel = "nightly", -- "stable" or "nightly"
+		-- channel = "nightly", -- "stable" or "nightly"
+		channel = "stable", -- "stable" or "nightly"
 		version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
 		branch = "main", -- branch name (NIGHTLY ONLY)
 		commit = nil, -- commit hash (NIGHTLY ONLY)
