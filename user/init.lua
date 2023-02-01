d8325b1c319a185f5dfa06781e7cb50f5936efd9
sjava <515957966@qq.com>
2023-02-01 18:37:39 +0800

    update

--- user/init.lua
+++ user/init.lua
@@ -498,23 +498,6 @@ local config = {
 				end,
 			},
 			{ "bkad/CamelCaseMotion" },
-			{
-				"RRethy/nvim-treesitter-textsubjects",
-				after = "nvim-treesitter",
-				config = function()
-					require("nvim-treesitter.configs").setup({
-						textsubjects = {
-							enable = true,
-							prev_selection = ",",
-							keymaps = {
-								["."] = "textsubjects-smart",
-								[";"] = "textsubjects-container-outer",
-								["i;"] = "textsubjects-container-inner",
-							},
-						},
-					})
-				end,
-			},
 		},
 		-- cmp = function(opts)
 		-- end,
